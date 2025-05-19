bbcb7b3160df032e0d232441cbad3dc355ea536a
diego <202312413h@utea.edu.pe>
2025-05-19 16:25:25 +0200

    ejercicio1

---
+++ Ejercicios1/Ejercicio2.java
@@ -0,0 +1,10 @@
+package Ejercicios1;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Diego Sanchez");
+        System.out.println("Dirección: Barrio Santa Rosa, Abancay, Peru");
+        System.out.println("Teléfono: +51993231701");
+    }
+}
