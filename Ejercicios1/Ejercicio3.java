bc834f92c8804be4366e45281264e8f82476eb22
diego <202312413h@utea.edu.pe>
2025-05-22 10:33:01 -0500

    Ejercicio3

--- Ejercicios1/Ejercicio3.java
+++ Ejercicios1/Ejercicio3.java
@@ -1,26 +1,17 @@
+package Ejercicios1;
+
 public class Ejercicio3 {
+
     public static void main(String[] args) {
-        // Arreglo de palabras en inglés y español
-        String[][] palabras = {
-            {"computer", "ordenador"},
-            {"student", "alumno/a"},
-            {"cat", "gato"},
-            {"penguin", "pingüino"},
-            {"machine", "máquina"},
-            {"nature", "naturaleza"},
-            {"light", "luz"},
-            {"green", "verde"},
-            {"book", "libro"},
-            {"pyramid", "pirámide"}
-        };
-
-        // Imprimir cabecera
-        System.out.printf("%-10s %-15s\n", "Inglés", "Español");
-        System.out.println("------------------------------");
-
-        // Imprimir palabras alineadas
-        for (int i = 0; i < palabras.length; i++) {
-            System.out.printf("%-10s %-15s\n", palabras[i][0], palabras[i][1]);
-        }
-    }
+        System.out.println("computer\tordenador");
+        System.out.println("atudent\t\talumno");
+        System.out.println("cat\t\tgato");
+        System.out.println("penguin\t\tpinguiino");
+        System.out.println("machine\t\tmaquina");
+        System.out.println("nature\t\tnaturaleza");
+        System.out.println("light\t\tluz");
+        System.out.println("green\t\tverde");
+        System.out.println("book\t\tlibro");
+        System.out.println("pyramid\t\tpiramide");
+}
 }
