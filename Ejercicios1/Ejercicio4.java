85ac58df9791ab4349758ed7a8c16b7a154e0f41
diego <202312413h@utea.edu.pe>
2025-05-22 10:53:45 -0500

    Ejercicio4

---
+++ Ejercicios1/Ejercicio4.java
@@ -0,0 +1,14 @@
+package Ejercicios1;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println("Lunes\tMartes\t\tMiercoles\tJueves\t\tViernes");
+        System.out.println("=====\t======\t\t==========\t=====\t\t=======");
+        System.out.println("clase\tclase\t\tclase\t\tclase\t\tclase"); 
+        System.out.println("dota\tjuego\t\tclase\t\tdota\t\tdota");
+        System.out.println("futbol\tclase\t\tclase\t\tnada\t\tclase");
+        System.out.println("futbol\tclase\t\tclase\t\tnada\t\tclase");
+        System.out.println("clase\tclase\t\tclase\t\tclase\t\tclase");
+    }
+    
+}
